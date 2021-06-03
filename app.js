b54b48e6d1121cf2a7b8222e277fa4168d104b78
“Eduard <3du4rd@gmail.com>
2021-06-03 00:16:14 -0500

    feat: 70 Serving HTML Pages

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require ('express');
 const bodyParser = require('body-parser');
 
@@ -37,7 +39,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use( (req,res,next) => {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -9,7 +10,8 @@ router.use('/users', (req, res, next) => {
 
 router.get('/add-product', (req, res, next) => {
     console.log('Add Product');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit"/>Add Product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit"/>Add Product</button></form>');
 }); 
 
 router.post('/add-product', (req, res, next) => {
