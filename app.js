612edcd744cc2b82827185e4104d31792a7d59fd
“Eduard <3du4rd@gmail.com>
2021-06-03 00:39:50 -0500

    feat: 75 Serving Files Statically

--- app.js
+++ app.js
@@ -8,32 +8,8 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// app.use( (req, res, next) => {
-//     console.log('In the middleware 1!');
-//     next();
-// });
-
-// app.use( (req, res, next) => {
-//     console.log('In the middleware 2!');
-//     res.send('<h1>My First Express Assigment !</h1>');
-// });
-
-// app.use('/users', (req, res, next) => {
-//     console.log('Users Page');
-//     res.send('<h1>The Users Pages</h1>');
-// });
-
-// app.use('/add-product', (req, res, next) => {
-//     console.log('Add Product');
-//     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"/>Add Product</button></form>');
-// }); 
-
-// app.post('/product', (req, res, next) => {
-//     console.log(req.body);
-//     res.redirect('/');
-// });
-
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
