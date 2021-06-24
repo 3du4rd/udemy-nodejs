58fe1ee8abf29b50e20a1ae4d0c91f097346d0cd
“Eduard <3du4rd@gmail.com>
2021-06-24 00:33:09 -0500

    ci: ajustes de configuracion para despliegue en Heroku

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 const express = require ('express');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+
 
 const app = express();
 app.set('view engine','pug');
@@ -22,4 +25,4 @@ app.use( (req,res,next) => {
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
