9d2254f26dfb25c1e5a34f3bcffaba206f86adf9
“Eduard <3du4rd@gmail.com>
2021-07-29 01:13:28 -0500

    feat: 123. Adding a Cart Model

--- models/product.js
+++ models/product.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename),
+  path.dirname(require.main.filename), //process.mainModule.filename),
   'data',
   'products.json'
 );
