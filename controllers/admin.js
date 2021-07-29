367d5681ba6e9dc9982e6d30c99257c03e07569c
“Eduard <3du4rd@gmail.com>
2021-07-29 01:23:44 -0500

    feat: 124. Using Query Params

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,12 +7,9 @@ const Product = require('../models/product')
  * @param {*} next 
  */
  exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product (Ejs)',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        path: '/admin/add-product'
     });
 };
 
@@ -33,6 +30,19 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);    
     product.save();
     res.redirect('/');
+};
+
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode){
+        return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+        pageTitle: 'Edit Product (Ejs)',
+        path: '/admin/edit-product',
+        editing: editMode
+    });
 };
 
 /**
--- routes/admin.js
+++ routes/admin.js
@@ -18,4 +18,7 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 module.exports = router;
