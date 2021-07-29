86d37e7ea0be27be22d46314967c591ddd94e73e
“Eduard <3du4rd@gmail.com>
2021-07-29 01:41:12 -0500

    feat: 125. Pre-Populating the Edit Product Page with

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,7 +9,8 @@ const Product = require('../models/product')
  exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product (Ejs)',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        editing: false
     });
 };
 
@@ -34,15 +35,22 @@ exports.postAddProduct = (req, res, next) => {
 
 
 exports.getEditProduct = (req, res, next) => {
-    const editMode = req.query.edit;
+    const editMode = new Boolean(req.query.edit);
     if (!editMode){
         return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-        pageTitle: 'Edit Product (Ejs)',
-        path: '/admin/edit-product',
-        editing: editMode
-    });
+    const prodId = req.params.productId;
+    Product.findById( prodId, product => {
+        if(!product){
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product (Ejs)',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        });
+    });    
 };
 
 /**
