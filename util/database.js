1dc443dd7f0a3fd9a5a870d746f5ce40e7c7e6a3
3du4rd <3du4rd@gmail.com>
2021-08-02 12:20:18 -0500

    :lock: Security configuration

--- util/database.js
+++ util/database.js
@@ -1,11 +1,12 @@
 const Pool = require('pg').Pool;
+require('dotenv').config()
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '***REMOVED***',
-  database: 'postgres',
-  password: '***REMOVED***',
-  port: 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
 });
 
 module.exports = pool;
