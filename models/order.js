1ca887e6da840888b59b098a58a61361ef2b14ca
3du4rd <3du4rd@gmail.com>
2021-08-02 23:02:22 -0500

    feat: 171. Adding an Order Model

---
+++ models/order.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../util/database');
+
+const Order = sequelize.define('order',{
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  }  
+},{
+  timestamps: true
+});
+
+module.exports = Order;
+
