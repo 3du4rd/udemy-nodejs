01bc153635e11f099c58fd7d4c0677f664fd15b8
3du4rd <3du4rd@gmail.com>
2021-08-02 21:17:39 -0500

    feat: 162.Adding a One-To-Many Relationship

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require ('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+
+const Product = require('./models/product');
+const User = require('./models/user');
  
 const PORT = process.env.PORT || 5000;
 
@@ -26,7 +29,18 @@ app.use('/api',apiRoutes.routes);
 
 app.use(errorController.get404);
 
-sequelize.sync({ alter: true })
+
+//-> DB relationship
+Product.belongsTo(User,{
+    constraints: true,
+    onDelete: 'CASCADE'
+});
+User.hasMany(Product);
+
+sequelize.sync({ 
+    alter: true,
+    force: true 
+})
 .then(result => {
     //console.log(result);
     app.listen(PORT, () => console.log(`Listening on ${PORT}`));
