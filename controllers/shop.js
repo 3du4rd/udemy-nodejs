3b4cd27804f9993c83f366a7f7864df55d374f41
3du4rd <3du4rd@gmail.com>
2021-08-05 12:53:35 -0500

    feat: :sparkles: 227. Creating, Getting & Displaying the Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product')
-//const Order = require('../models/order')
+const Order = require('../models/order')
 
 /**
  * Permite obtener todos los productos de la base de datos 
@@ -125,24 +125,42 @@ exports.postCartDeleteProduct = (req, res, next) => {
 
 exports.postOrder = (req, res, next) => {
   req.user
-    .addOrder()
-    .then(result => {
-      res.redirect('/orders');
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map (i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();      
     })
-    .catch(err => console.log(err));
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })       
+    .catch(err => { 
+      console.log(err)
+    });
 };
 
  exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
+   Order.find({'user.userId': req.user._id})
+   .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
 
 /**
