347df5a23d29876151637128880be630321f35f7
3du4rd <3du4rd@gmail.com>
2021-08-05 23:35:58 -0500

    feat: :sparkles: 243. Deleting a Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,3 +29,10 @@ exports.postLogin = (req, res, next) => {
         });
   }
 };
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
