0a33942d4af8dcd3d49c8eb885a60434fbe8f0f5
3du4rd <3du4rd@gmail.com>
2021-08-06 18:59:52 -0500

    feat: :sparkles: 258. Working on Route Protection

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,12 +7,15 @@ const Product = require('../models/product');
  * @param {*} next 
  */
  exports.getAddProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-        pageTitle: 'Add Product (Ejs)',
-        path: '/admin/add-product',
-        editing: false,
-        isAuthenticated: req.session.isLoggedIn        
-    });
+     if (!req.session.isLoggedIn) {
+         return res.redirect('/login');
+     }
+     res.render('admin/edit-product', {
+         pageTitle: 'Add Product (EJS)',
+         path: '/admin/add-product',
+         editing: false,
+         isAuthenticated: req.session.isLoggedIn
+     });
 };
 
 /**
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,8 +2,6 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user'); 
 
 exports.getLogin = (req, res, next) => {
-  //console.log(req.get('Cookie'));
-  console.log('User isLoggedIn: ' + req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
