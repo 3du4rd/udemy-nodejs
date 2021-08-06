fcfe6ad228fd87bc7bf1f8d8c9940d74b71eb407
3du4rd <3du4rd@gmail.com>
2021-08-05 21:48:50 -0500

    feat: :sparkles: 233. Creating the Login Form

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('views','views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 // const apiRoutes = require('./routes/api');
 
 //app.use(bodyParser.urlencoded({extended: false}));
@@ -34,6 +35,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // app.use('/api',apiRoutes.routes);
 
