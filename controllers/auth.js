8e20e918c7653c8d627542ff3ee7d13d02d443d2
3du4rd <3du4rd@gmail.com>
2021-08-05 22:35:54 -0500

    feat: :sparkles: 240. Using the Session Middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie'));
+  //console.log(req.get('Cookie'));
+  console.log('User isLoggedIn: ' + req.session.isLoggedIn);
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
@@ -8,6 +9,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie','loggedIn=true; Max-Age=60; HttpOnly');
+  //res.setHeader('Set-Cookie','loggedIn=true; Max-Age=60; HttpOnly');
+  req.session.isLoggedIn = true;
   res.redirect('/');
 };
