44f8712a0ad2d875894df567a59ce23f51f1483a
3du4rd <3du4rd@gmail.com>
2021-08-06 18:10:47 -0500

    fix(security): :lock: 255. Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user'); 
 
 exports.getLogin = (req, res, next) => {
@@ -42,6 +43,8 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
+  //TODO 1. Encrypt password
+  //TODO 2. Validate inputs
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
@@ -50,9 +53,12 @@ exports.postSignup = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup');
       }
+      return bcrypt.hash(password, 12);      
+    })
+    .then(hashedPassword => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] }
       });
       return user.save();
