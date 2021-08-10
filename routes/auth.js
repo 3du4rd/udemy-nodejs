45829335b99604903a91bb0cccaee8df58b66577
3du4rd <3du4rd@gmail.com>
2021-08-10 15:50:18 -0500

    feat: :sparkles: 293. Built-In & Custom Validators

--- routes/auth.js
+++ routes/auth.js
@@ -13,7 +13,15 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post('/signup', 
-    check('email').isEmail().withMessage('Please enter a valid email.'), 
+    check('email')
+    .isEmail()
+    .withMessage('Please enter a valid email.')
+    .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address is forbidden');
+        }
+        return true;
+      }), 
     authController.postSignup);
 
 router.post('/logout', authController.postLogout);
