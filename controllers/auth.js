bfffe948d560a79932c2b4c4857e8a0f9f056257
3du4rd <3du4rd@gmail.com>
2021-08-10 18:24:50 -0500

    feat: :sparkles: 297. Keeping User Input

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,12 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
   });
 };
 
@@ -87,7 +92,8 @@ exports.postSignup = (req, res, next) => {
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email:email, password:password, confirmPassword: req.body.confirmPassword }
     });
   }
   bcrypt.hash(password, 12)
