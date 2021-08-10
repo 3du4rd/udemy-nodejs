6e8ee58f4e10c0f42758caee1f436214d6a797b8
3du4rd <3du4rd@gmail.com>
2021-08-10 00:58:34 -0500

    feat: :sparkles: 291. Setup & Basic Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const mailgun = require("mailgun-js");
+const { validationResult } = require('express-validator/check');
+
 const User = require('../models/user'); 
 
 exports.getLogin = (req, res, next) => {
@@ -71,6 +73,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
