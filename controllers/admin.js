6c728acae391628ec173cf149c0ae35866f6a586
3du4rd <3du4rd@gmail.com>
2021-08-12 00:11:47 -0500

    feat: :sparkles: 350. Sending & Handling Background Requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -193,8 +193,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 
-exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
     Product.findById(prodId)
       .then(product => {
         if (!product) {
@@ -205,11 +205,13 @@ exports.postDeleteProduct = (req, res, next) => {
       })
       .then(() => {
         console.log('DESTROYED PRODUCT');
-        res.redirect('/admin/products');
+        res.status(200).json({
+            message: 'Success!'
+        });
       })
       .catch(err => {
-        const error = new Error(err);
-        error.httpStatusCode = 500;
-        return next(error);
+        res.status(500).json({
+            message: 'Failed'
+        });
       });
   };
--- routes/admin.js
+++ routes/admin.js
@@ -59,6 +59,8 @@ router.post(
   isAuth,
   adminController.postEditProduct
 );
-router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+
+//router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
 
 module.exports = router;
