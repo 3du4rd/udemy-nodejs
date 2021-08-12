df3654d178b1de10734e8c90fe1a2316a47f7bbb
3du4rd <3du4rd@gmail.com>
2021-08-11 23:21:31 -0500

    feat: :sparkles: 341. Preparing Pagination Data on the Server

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,15 +68,27 @@ exports.getProduct = (req, res, next) => {
  */
 exports.getIndex = (req, res, next) => {
   const page = req.query.page;
+  let totalItems;
 
   Product.find()
-  .skip((page - 1) * ITEMS_PER_PAGE)
-  .limit(ITEMS_PER_PAGE)
+  .count()
+  .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+  })
   .then(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop Index - NoSQL with MongoDB',
-      path: '/'
+      path: '/',
+      totalProducts: totalItems,
+      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+      hasPreviousPage: page > 1,
+      nextPage: page + 1,
+      previousPage: page - 1,
+      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     });
   })
   .catch(err =>{
