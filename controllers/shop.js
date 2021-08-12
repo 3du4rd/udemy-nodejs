6c97c6ab57cf48d9521b31531bebf2e0116ddea9
3du4rd <3du4rd@gmail.com>
2021-08-11 23:38:14 -0500

    feat: :sparkles: 343. Re-Using the Pagination Logic & Controls

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,16 +15,31 @@ const ITEMS_PER_PAGE = 3;
  * @param {*} next 
  */
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
 
   Product.find()
+    .count()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'Shop - Product List (Sequelize)',
+        pageTitle: 'Shop - Product List',
         path: '/products',
         hasProducts: products > 0,
         activeShop: true,
-        productCSS: true
+        productCSS: true,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch(err =>{
